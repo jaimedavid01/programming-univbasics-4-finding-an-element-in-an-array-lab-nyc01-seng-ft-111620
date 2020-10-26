01eee6b071298c5fc46ae1481551eb218ea7e982
Jaime David <jaimedavid01@yahoo.com>
2020-10-26 05:03:26 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,3 @@
-def find_element_index(array, value_to_find)
- counter = 0 
- while counter == value_to_find
- puts counter
- counter +=1
- else
-   puts nil
-end
 
 def find_element_index(array, value_to_find)
   counter = 0 
